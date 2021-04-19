137d74678bdf2514d57efba0cec697d78240ec88
streikerpz <78185251+streikerpz@users.noreply.github.com>
2021-04-19 12:01:59 -0600

    Add files via upload

---
+++ Calculadora.BL/Validaciones/Tangente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.BL.Validaciones
+{
+    // amplíe la capacidad de esta clase para que funcione con radianes
+    public class Tangente
+    {
+        public bool EstaDefinida (double cantidadDeGrados)
+        {
+            var resultado = Math.Abs(cantidadDeGrados % 180) != 90;
+            return resultado;
+        }
+
+        /// <summary>
+        /// Devuelve la tangente de un número. Cuando el número es de la forma (2n+1)pi/2, devuelve null
+        /// </summary>
+        /// <param name="cantidadDeGrados">Cantidad en grados</param>
+        /// <returns>La tangente del número</returns>
+        public double? RealizarCalculo (double cantidadDeGrados)
+        {
+            double? resultado = null;
+            if (EstaDefinida (cantidadDeGrados))
+                resultado =  (Math.Tan(cantidadDeGrados));
+            return resultado;
+        }
+    }
+}
